ba73873af730cf5bfa4e637393961562173b5d9f
wchoEvo <32346245+wchoEvo@users.noreply.github.com>
2021-04-17 13:55:55 -0700

    Create calculate_both_hmm.sh

---
+++ MURI2/201_hmm/hmm_all_analysis/calculate_both_hmm.sh
@@ -0,0 +1,3 @@
+cd /Users/wei-chinho/Documents/MURI2/201_hmm/hmm_all_analysis
+python calculate_well_mixed_hmm_wrapper.py > calculate_well_mixed_hmm_wrapper.o 2> calculate_well_mixed_hmm_wrapper.e
+python calculate_clade_hmm_wrapper.py > calculate_clade_hmm_wrapper.o 2> calculate_clade_hmm_wrapper.e
